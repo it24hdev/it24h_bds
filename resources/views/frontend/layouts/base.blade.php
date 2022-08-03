52e92f1756a51a69f9a71b9a846b449258f07ed2
Trung Dung <dungtdcs@gmail.com>
2022-08-03 17:06:45 +0700

    content home

--- resources/views/frontend/layouts/base.blade.php
+++ resources/views/frontend/layouts/base.blade.php
@@ -11,6 +11,8 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <link rel="stylesheet" href="/asset/lib/fontawesomePro5/css/all.min.css">
+    <link rel="stylesheet" href="/asset/lib/OwlCarousel/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="/asset/lib/OwlCarousel/dist/assets/owl.theme.default.min.css">
 	<link rel="stylesheet" href="{{asset('asset/css/header-home.css')}}">
 	<link rel="stylesheet" href="{{asset('asset/css/content-home.css')}}">
     @yield('css')
@@ -21,6 +23,9 @@
 	@yield('content')
 	@yield('footer')
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="/asset/lib/OwlCarousel/dist/owl.carousel.min.js"></script>
     @yield('js')
 </body>
 </html>
