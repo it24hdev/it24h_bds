6abaf2d56b3e9ac23437a76499eb9c1d53072578
Trung Dung <dungtdcs@gmail.com>
2022-08-04 15:39:43 +0700

    no message

--- resources/views/frontend/layouts/base.blade.php
+++ resources/views/frontend/layouts/base.blade.php
@@ -24,13 +24,11 @@
 	@yield('content')
 	@yield('footer')
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="/asset/lib/OwlCarousel/dist/owl.carousel.min.js"></script>
     @yield('js')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-
     <script src="{{ asset('asset/js/header-home.js') }}"></script>
 </body>
 </html>
